f53915dfafbd6915fd9aa3b092132ce3addc8e64
nirs <sheep.nir@gmail.com>
2024-03-17 19:55:21 -0700

    Updating app.py

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask import Flask, render_template
 
+# Create a Flask application instance
 app = Flask(__name__)
 
+# Define the home route ("/")
 @app.route('/')
 def home():
+    # Render the 'home.html' template and return it as the response
     return render_template('home.html')
 
+# Define the about-us route ("/about-us")
 @app.route('/about-us')
 def about_us():
+    # Render the 'about.html' template and return it as the response
     return render_template('about.html')
 
+# Check if the script is running as the main program
 if __name__ == '__main__':
+    # Run the Flask application
+    # Set the host to '0.0.0.0' to make the app accessible from any IP address
+    # Set the port to 5000, which is the default port for Flask
     app.run(host='0.0.0.0', port=5000)
