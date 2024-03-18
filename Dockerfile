390f5c2e13f689479be7cb2e3861126a42f082b6
nirs <sheep.nir@gmail.com>
2024-03-17 19:53:08 -0700

    Adding a bootstrap template

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
+COPY templates templates
 
 EXPOSE 5000
 
--- app.py
+++ app.py
@@ -1,23 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
-# Create a Flask application instance
 app = Flask(__name__)
 
-# Define the root route ("/")
 @app.route('/')
-def hello():
-    # Return the greeting message
-    return 'Hello from Flask and Docker'
+def home():
+    return render_template('home.html')
 
-# Define the "/about-us" route
 @app.route('/about-us')
 def about_us():
-    # Return the about-us message
-    return 'This is a test app'
+    return render_template('about.html')
 
-# Check if the script is running as the main program
 if __name__ == '__main__':
-    # Run the Flask application
-    # Set the host to '0.0.0.0' to make the app accessible from any IP address
-    # Set the port to 5000, which is the default port for Flask
     app.run(host='0.0.0.0', port=5000)
